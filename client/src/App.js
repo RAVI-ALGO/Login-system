fef8798f9e1bdce86f266323bac3be9e254adacb
RAVI-ALGO <kushwahark724@gmail.com>
2023-08-18 15:39:51 +0530

    18/08/2023

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import LoginPage from './components/LoginPage';
+import Register from './components/Register';
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      React App
-    </div>
+    <>
+      <Routes>
+      <Route exact path='/' element={<LoginPage />} />
+      <Route exact path='/register' element={<Register />} />
+      </Routes>
+    </>
   );
 }
 
